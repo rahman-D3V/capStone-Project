cbdb22253da1bf2760a854fac090c65fdf085c83
rahman-D3V <yr309710@gmail.com>
2026-03-12 13:14:50 +0530

    Initial commit: set up capstone project with basic landing page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FraudShieldLanding from './components/LandingPage'
+
+const App = () => {
+  return (
+    <div>
+      <FraudShieldLanding/>
+    </div>
+  )
+}
+
+export default App
